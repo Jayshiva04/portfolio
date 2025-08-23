eef009c7ddc162646fbdfa4a291183c8c93b065d
Jayshiva04 <jayshivat02@gmail.com>
2025-08-24 01:42:15 +0530

    feat: setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+// vite.config.ts
+
+import path from "path" // 👈 Add this import
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👇 Add this 'resolve' block
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
